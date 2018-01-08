8fa8352d7e0d36c5c6a768c25f90c9329167d504
Amir Hadad <amir.hadad@me.com>
2018-01-09 10:28:28 +1100

    updated appliction user fields.

---
+++ src/main/kotlin/com/demo/ApplicationUserController.kt
@@ -0,0 +1,19 @@
+package com.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ApplicationUserController(private val repository: ApplicationUserRepository) {
+
+	@GetMapping("/app/users")
+	fun findAll() = repository.findAll()
+
+	@GetMapping("/app/users/{lastName}")
+	fun findByUsername(@PathVariable username:String)
+			= repository.findByUsername(username)
+	@GetMapping("/app/users/{email}")
+	fun findByEmail(@PathVariable email:String)
+			= repository.findByEmail(email)
+}
